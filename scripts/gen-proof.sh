a9df11f12db10dd9171ce84e974bdf1d4f07bd06
CW 4.0 <jsonwisdom@yahoo.com>
2026-04-21 17:11:27 -0500

    Add directional Merkle proof generation

--- scripts/gen-proof.sh
+++ scripts/gen-proof.sh
@@ -16,45 +16,61 @@ if [ ! -d "$TREE_DIR" ]; then
 fi
 
 proof=()
-current_file="$TREE_DIR/level_0.txt"
+current_hash="$LEAF"
+level=0
 
 while true; do
+  current_file="$TREE_DIR/level_${level}.txt"
+  next_file="$TREE_DIR/level_$((level + 1)).txt"
+
+  if [ ! -f "$current_file" ]; then
+    echo "Missing tree level: $current_file"
+    exit 1
+  fi
+
   mapfile -t lines < "$current_file"
   found_index=-1
 
   for i in "${!lines[@]}"; do
     hash=$(echo "${lines[$i]}" | awk '{print $1}')
-    if [ "$hash" = "$LEAF" ]; then
+    if [ "$hash" = "$current_hash" ]; then
       found_index=$i
       break
     fi
   done
 
   if [ "$found_index" -eq -1 ]; then
-    break
+    echo "Leaf/hash not found at level $level"
+    exit 1
   fi
-
-  if [ $((found_index % 2)) -eq 0 ]; then
-    sibling_index=$((found_index + 1))
-  else
-    sibling_index=$((found_index - 1))
-  fi
-
-  if [ "$sibling_index" -lt ${#lines[@]} ]; then
-    sibling=$(echo "${lines[$sibling_index]}" | awk '{print $1}')
-    proof+=("$sibling")
-  fi
-
-  next_level=$(basename "$current_file" | sed 's/level_//;s/.txt//')
-  next_level=$((next_level + 1))
-  next_file="$TREE_DIR/level_${next_level}.txt"
 
   if [ ! -f "$next_file" ]; then
     break
   fi
 
-  LEAF=$(printf "%s%s" "$LEAF" "$sibling" | sha256sum | awk '{print $1}')
-  current_file="$next_file"
+  if [ $((found_index % 2)) -eq 0 ]; then
+    sibling_index=$((found_index + 1))
+    direction="R"
+  else
+    sibling_index=$((found_index - 1))
+    direction="L"
+  fi
+
+  if [ "$sibling_index" -ge ${#lines[@]} ]; then
+    sibling_hash=$(echo "${lines[$found_index]}" | awk '{print $1}')
+  else
+    sibling_hash=$(echo "${lines[$sibling_index]}" | awk '{print $1}')
+  fi
+
+  proof+=("${direction}:${sibling_hash}")
+
+  if [ "$direction" = "R" ]; then
+    current_hash=$(printf "%s%s" "$current_hash" "$sibling_hash" | sha256sum | awk '{print $1}')
+  else
+    current_hash=$(printf "%s%s" "$sibling_hash" "$current_hash" | sha256sum | awk '{print $1}')
+  fi
+
+  level=$((level + 1))
 done
 
 printf "%s\n" "${proof[@]}"
